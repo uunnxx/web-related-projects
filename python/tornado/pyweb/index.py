8c12e94f66a93e0922dc327c6bf0aa7212bbccba
バカです <naimjonn.nn@live.ru>
2024-05-19 23:27:11 +0300

    Basic `GET` endpoint on root

--- python/tornado/pyweb/index.py
+++ python/tornado/pyweb/index.py
@@ -1,1 +1,20 @@
 import tornado.web
+import tornado.ioloop
+
+
+class BasicRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write('Hello, World')
+
+
+if __name__ == '__main__':
+    app = tornado.web.Application([
+        (r'/', BasicRequestHandler)
+    ])
+
+    port = 8008
+    app.listen(port)
+
+    print(f'Listining on {port = }')
+
+    tornado.ioloop.IOLoop.current().start()
