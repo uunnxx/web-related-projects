85c5c56b2da66f7a16dcced546103557f3e9b85a
バカです <naimjonn.nn@live.ru>
2024-05-19 20:34:36 +0300

    Account application

---
+++ python/django_drf/django4_onru/bookmarks/account/views.py
@@ -0,0 +1,55 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
+from .forms import LoginForm
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request,
+                username=cd['username'],
+                password=cd['password']
+            )
+
+            # if user is None:
+            #     return HttpResponse('Invalid login')
+            #
+            # if user.is_active():
+            #     login(request, user)
+            #     return HttpResponse('Authenticated successfully')
+            # else:
+            #     return HttpResponse('Disabled account')
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+
+    return render(request, 'account/login.html', {'form': form})
+
+
+# @login_required
+# def user_logout(request):
+#     logout(request)
+#     return render(request, 'registration/logged_out.html', {})
+
+
+@login_required
+def dashboard(request):
+    return render(
+        request,
+        'account/dashboard.html',
+        {'section': 'dashboard'}
+    )
