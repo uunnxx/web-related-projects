bbf6d31b847d3dc83480521bf269407e75fb2580
バカです <naimjonn.nn@live.ru>
2024-05-19 23:40:37 +0300

    Basic `GET` endpoints with parameters

--- python/tornado/pyweb/index.py
+++ python/tornado/pyweb/index.py
@@ -7,9 +7,32 @@ class BasicRequestHandler(tornado.web.RequestHandler):
         self.write('Hello, World')
 
 
+class ListRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('index.html')
+
+
+class QueryParamRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        num = self.get_argument('num')
+        if (num.isdigit()):
+            result = 'odd' if int(num) & 1 else 'even'
+            self.write(f'The integer {num} is {result}')
+        else:
+            self.write(f'{num} is not a valid integer')
+
+
+class ResourceParamRequestHandler(tornado.web.RequestHandler):
+    def get(self, student_name, course_id):
+        self.write(f'Welcome {student_name} the course you are viewing is {course_id}')
+
+
 if __name__ == '__main__':
     app = tornado.web.Application([
-        (r'/', BasicRequestHandler)
+        (r'/', BasicRequestHandler),
+        (r'/animal', ListRequestHandler),
+        (r'/isEven', QueryParamRequestHandler),
+        (r'/students/([A-Za-z]+)/([0-9]+)', ResourceParamRequestHandler),
     ])
 
     port = 8008
