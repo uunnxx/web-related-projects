9e72a4d4153e7db72600b6a0318b3bd7e69763b6
バカです <naimjonn.nn@live.ru>
2024-06-07 21:16:55 +0300

    Project related

---
+++ python/django_drf/impatient/moviereviews/moviereviews/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+from movie import views as movieViews
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', movieViews.home, name='home'),
+    path('about/', movieViews.about, name='about'),
+    path('signup/', movieViews.signup, name='signup'),
+    path('news/', include('news.urls')),
+    path('movie/', include('movie.urls'))
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
